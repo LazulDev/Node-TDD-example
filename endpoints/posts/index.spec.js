595e28912e6c616de25d4e63ff68b123a1c97f2e
María Sánchez <msanchez.telecom@gmail.com>
2020-09-28 17:41:28 +0200

    posts endpoint test done

--- endpoints/posts/index.spec.js
+++ endpoints/posts/index.spec.js
@@ -1,8 +1,42 @@
+const postHandlers = require('./index')
 describe('Endpoints', () => {
     describe('post', () => {
-        it.skip('should create', async () => {
+        it('should create', async () => {
+            const mockUsers = [
+                { id: 1 },
+                { id: 2 },
+            ]
+            const post = {
+                userId: 1,
+                id: 1,
+                title: "Titulo",
+                body: "Cuerpo del post"
+            } 
+            const req = {
+                body: post,
+            }
+            const res = {
+                status: jest.fn(),
+                send: jest.fn(),
+            }
+            const axios = {
+                get: jest.fn().mockResolvedValue({ data: mockUsers }),
+                post: jest.fn().mockResolvedValue({ data: { id: 1000 }}),
+            }
 
-            
+            await postHandlers({ axios }).post(req, res)
+            expect(res.status.mock.calls).toEqual([
+                [201] // Creado
+            ])
+            expect(res.send.mock.calls).toEqual([
+                [{ id: 1000 }]
+            ])
+            expect(axios.get.mock.calls).toEqual([
+                ['https://jsonplaceholder.typicode.com/posts']
+            ])
+            expect(axios.post.mock.calls).toEqual([
+                ['https://jsonplaceholder.typicode.com/posts', post ]
+            ])
         });
         
     });
